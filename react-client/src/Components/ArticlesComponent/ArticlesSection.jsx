440eda0f82b3568496e7cff850d6102c8fb5a46f
CDAP <corypitt@mac.com>
2020-03-22 12:31:06 -0700

    Article Section

---
+++ react-client/src/Components/ArticlesComponent/ArticlesSection.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class ArticlesSection extends Component {
+    render() {
+        return (
+            <div>
+                Here are some articles I've written:
+                <ol>Data Strategies</ol>
+            </div>
+        )
+    }
+}
+export default ArticlesSection;
