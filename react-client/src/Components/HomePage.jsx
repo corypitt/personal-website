49d19de288844386804238301684153593a2dda7
CDAP <corypitt@mac.com>
2020-03-21 17:21:56 -0700

    Initial commit

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                Home page
+                This is a test!
+            </div>
+        )
+    }
+}
+export default HomePage;
